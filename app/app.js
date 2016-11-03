b74a0f56ea7e223274acc892df86c49df3dbf77b
litao <litaowork@aliyun.com>
2016-11-03 17:32:19 +0800

    增加导航条

--- app/app.js
+++ app/app.js
@@ -20,6 +20,85 @@ export default class App extends Component {
 	constructor(props) {
 		super(props);
 	}
+	// 渲染导航条
+	_renderNavBar() {
+		const styles = {
+			title: {
+				flex: 1,
+				alignItems: 'center',
+				justifyContent: 'center'
+			},
+			button: {
+				flex: 1,
+				width: 50,
+				alignItems: 'center',
+				justifyContent: 'center'
+			},
+			buttonText: {
+				fontSize: 18,
+				color: '#FFFFFF',
+				fontWeight: '400'
+			}
+		}
+
+		let routeMapper = {
+			LeftButton(route, navigator, index, navState) {
+				if (index > 0) {
+					return (
+						<TouchableOpacity 
+			              	onPress={() => navigator.pop()}
+			              	style={styles.button}>
+			              	<Text style={styles.buttonText}>Back</Text>
+	            		</TouchableOpacity>
+					);
+				} else {
+					return (
+						<TouchableOpacity 
+			              	onPress={() => navigator.pop()}
+			              	style={styles.button}>
+			              	<Text style={styles.buttonText}>Logo</Text>
+	            		</TouchableOpacity>
+					);
+				}
+			},
+			RightButton(route, navigator, index, navState) {
+				if (index > 0 && route.rightButton) {
+					return (
+						<TouchableOpacity 
+			              	onPress={() => navigator.pop()}
+			              	style={styles.button}>
+			              	<Text style={styles.buttonText}></Text>
+			            </TouchableOpacity>
+					);
+				} else {
+					return null
+				}
+
+			},
+			Title(route, navigator, index, navState) {
+				return (
+					<View style={styles.title}>
+	            		<Text style={styles.buttonText}>{route.name ? route.name : '首页'}</Text>
+	          		</View>
+				);
+			}
+		};
+		return (
+	      	<Navigator.NavigationBar
+		        style={{
+		          	alignItems: 'center',
+		          	backgroundColor: '#55ACEE',
+		          	shadowOffset:{
+		              	width: 1,
+		              	height: 0.5,
+		          	},
+		          	shadowColor: '#55ACEE',
+		          	shadowOpacity: 0.8,          
+		        }}
+		        routeMapper={routeMapper}
+		    />
+	    );
+	}
 
 	render() {
 		return (
@@ -53,6 +132,8 @@ export default class App extends Component {
 						)
 					}
 				}
+				navigationBar = {this._renderNavBar.call(this)}
+				sceneStyle={{paddingTop: (Platform.OS === 'android' ? 60 : 74)}}
 			/>
 		)
 	}
